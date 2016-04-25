msantos/alcove

commit 8289f85ef4f32ff49c5441dbc23ed365a77c5793

alcove: modify handling of bad message headers
If an invalid message header was received, the process would call exit
and write to stderr:

    process 1 (bad hdr) -> process 2 (process exits) -> process 3
    process 1 <- process 2 (process exits) <- process 3 (bad hdr)

Avoid writing to stderr and treat the error in the same way as a write
error:

    process 1 (bad hdr) -> process 2 (write error, child stdout closed) -> process 3 (process exits)
    process 1 <- process 2 (write error, stdout closed) <- process 3 (bad hdr)

The semantics are similar but still not entirely correct. Possibly the
process should return 'badmsg' or 'badhdr' and force an exception.

c_src/alcove_event_loop.c
@@ -40,9 +40,9 @@ static int alcove_stdin(alcove_state_t *ap);
 static ssize_t alcove_msg_call(alcove_state_t *ap, unsigned char *buf,
         u_int16_t buflen);
 
-static size_t alcove_proxy_hdr(unsigned char *hdr, size_t hdrlen,
+static ssize_t alcove_proxy_hdr(unsigned char *hdr, size_t hdrlen,
         u_int16_t type, pid_t pid, size_t buflen);
-static size_t alcove_call_hdr(unsigned char *hdr, size_t hdrlen,
+static ssize_t alcove_call_hdr(unsigned char *hdr, size_t hdrlen,
         u_int16_t type, size_t buflen);
 
 static ssize_t alcove_child_stdio(int fdin, u_int16_t depth,
@@ -256,14 +256,14 @@ alcove_msg_call(alcove_state_t *ap, unsigned char *buf, u_int16_t buflen)
     return alcove_call_reply(ALCOVE_MSG_CALL, reply, rlen);
 }
 
-    static size_t
+    static ssize_t
 alcove_proxy_hdr(unsigned char *hdr, size_t hdrlen, u_int16_t type,
         pid_t pid, size_t buflen)
 {
     u_int16_t len = 0;
 
     if (hdrlen < 8)
-        err(EXIT_FAILURE, "alcove_proxy_hdr");
+        return -1;
 
     put_int16(sizeof(type) + sizeof(pid) + buflen, hdr); len = 2;
     put_int16(type, hdr+len); len += 2;
@@ -272,14 +272,14 @@ alcove_proxy_hdr(unsigned char *hdr, size_t hdrlen, u_int16_t type,
     return len;
 }
 
-    static size_t
+    static ssize_t
 alcove_call_hdr(unsigned char *hdr, size_t hdrlen, u_int16_t type,
         size_t buflen)
 {
     u_int16_t len = 0;
 
     if (hdrlen < 4)
-        err(EXIT_FAILURE, "alcove_call_hdr");
+        return -1;
 
     put_int16(sizeof(type) + buflen, hdr); len = 2;
     put_int16(type, hdr+len); len += 2;
@@ -336,6 +336,9 @@ alcove_child_stdio(int fdin, u_int16_t depth, alcove_child_t *c,
 
     hdrlen = alcove_proxy_hdr(hdr, sizeof(hdr), type, c->pid, n);
 
+    if (hdrlen < 0)
+        return -1;
+
     iov[0].iov_base = hdr;
     iov[0].iov_len = hdrlen;
     iov[1].iov_base = buf;
@@ -353,6 +356,9 @@ alcove_call_reply(u_int16_t type, char *buf, size_t len)
     u_int16_t hdrlen = 0;
 
     hdrlen = alcove_call_hdr(hdr, sizeof(hdr), type, len);
+
+    if (hdrlen < 0)
+        return -1;
 
     iov[0].iov_base = hdr;
     iov[0].iov_len = hdrlen;
@@ -375,8 +381,14 @@ alcove_call_fake_reply(pid_t pid, u_int16_t type, char *buf, size_t len)
 
     callhdrlen = alcove_call_hdr(callhdr, sizeof(callhdr), type, len);
 
+    if (callhdrlen < 0)
+        return -1;
+
     proxyhdrlen = alcove_proxy_hdr(proxyhdr, sizeof(proxyhdr),
             ALCOVE_MSG_PROXY, pid, callhdrlen + len);
+
+    if (proxyhdrlen < 0)
+        return -1;
 
     iov[0].iov_base = proxyhdr;
     iov[0].iov_len = proxyhdrlen;

commit e7fcafd5a66a7d218d5d7c56b4ff24fe42415502

alcove: indicate a bad message by returning 0
Commit 8289f85ef4f3 introduced a sign conversion error. Return 0 if the
passed in buf is too small to hold the header and make the function
return an unsigned value.

c_src/alcove_event_loop.c
@@ -40,9 +40,9 @@ static int alcove_stdin(alcove_state_t *ap);
 static ssize_t alcove_msg_call(alcove_state_t *ap, unsigned char *buf,
         u_int16_t buflen);
 
-static ssize_t alcove_proxy_hdr(unsigned char *hdr, size_t hdrlen,
+static size_t alcove_proxy_hdr(unsigned char *hdr, size_t hdrlen,
         u_int16_t type, pid_t pid, size_t buflen);
-static ssize_t alcove_call_hdr(unsigned char *hdr, size_t hdrlen,
+static size_t alcove_call_hdr(unsigned char *hdr, size_t hdrlen,
         u_int16_t type, size_t buflen);
 
 static ssize_t alcove_child_stdio(int fdin, u_int16_t depth,
@@ -256,14 +256,14 @@ alcove_msg_call(alcove_state_t *ap, unsigned char *buf, u_int16_t buflen)
     return alcove_call_reply(ALCOVE_MSG_CALL, reply, rlen);
 }
 
-    static ssize_t
+    static size_t
 alcove_proxy_hdr(unsigned char *hdr, size_t hdrlen, u_int16_t type,
         pid_t pid, size_t buflen)
 {
     u_int16_t len = 0;
 
     if (hdrlen < 8)
-        return -1;
+        return 0;
 
     put_int16(sizeof(type) + sizeof(pid) + buflen, hdr); len = 2;
     put_int16(type, hdr+len); len += 2;
@@ -272,14 +272,14 @@ alcove_proxy_hdr(unsigned char *hdr, size_t hdrlen, u_int16_t type,
     return len;
 }
 
-    static ssize_t
+    static size_t
 alcove_call_hdr(unsigned char *hdr, size_t hdrlen, u_int16_t type,
         size_t buflen)
 {
     u_int16_t len = 0;
 
     if (hdrlen < 4)
-        return -1;
+        return 0;
 
     put_int16(sizeof(type) + buflen, hdr); len = 2;
     put_int16(type, hdr+len); len += 2;
@@ -336,7 +336,7 @@ alcove_child_stdio(int fdin, u_int16_t depth, alcove_child_t *c,
 
     hdrlen = alcove_proxy_hdr(hdr, sizeof(hdr), type, c->pid, n);
 
-    if (hdrlen < 0)
+    if (hdrlen == 0)
         return -1;
 
     iov[0].iov_base = hdr;
@@ -357,7 +357,7 @@ alcove_call_reply(u_int16_t type, char *buf, size_t len)
 
     hdrlen = alcove_call_hdr(hdr, sizeof(hdr), type, len);
 
-    if (hdrlen < 0)
+    if (hdrlen == 0)
         return -1;
 
     iov[0].iov_base = hdr;
@@ -381,13 +381,13 @@ alcove_call_fake_reply(pid_t pid, u_int16_t type, char *buf, size_t len)
 
     callhdrlen = alcove_call_hdr(callhdr, sizeof(callhdr), type, len);
 
-    if (callhdrlen < 0)
+    if (callhdrlen == 0)
         return -1;
 
     proxyhdrlen = alcove_proxy_hdr(proxyhdr, sizeof(proxyhdr),
             ALCOVE_MSG_PROXY, pid, callhdrlen + len);
 
-    if (proxyhdrlen < 0)
+    if (proxyhdrlen == 0)
         return -1;
 
     iov[0].iov_base = proxyhdr;