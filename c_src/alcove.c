8b3c944dff28a7709fcff8cedb10d68d6dc4b398
Michael Santos <michael.santos@gmail.com>
2020-03-12 09:56:37 -0400

    alcove.c/alcove.h: re-format
    Reformatting individually since header re-ordering can break
    compilation.

--- c_src/alcove.c
+++ c_src/alcove.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2018, Michael Santos <michael.santos@gmail.com>
+/* Copyright (c) 2014-2020, Michael Santos <michael.santos@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -25,246 +25,231 @@ static void usage(void);
 
 extern char *__progname;
 
-    int
-main(int argc, char *argv[])
-{
-    alcove_state_t *ap = NULL;
-    int ch = 0;
-    char *fifo = NULL;
-    int boot = 1;
-    struct rlimit maxfd = {0};
+int main(int argc, char *argv[]) {
+  alcove_state_t *ap = NULL;
+  int ch = 0;
+  char *fifo = NULL;
+  int boot = 1;
+  struct rlimit maxfd = {0};
 
 #ifndef HAVE_SETPROCTITLE
-    spt_init(argc, argv);
+  spt_init(argc, argv);
 #endif
 
-    ap = calloc(1, sizeof(alcove_state_t));
-    if (ap == NULL)
+  ap = calloc(1, sizeof(alcove_state_t));
+  if (ap == NULL)
+    exit(ENOMEM);
+
+  ALCOVE_SETOPT(ap, alcove_opt_termsig, 1);
+  ALCOVE_SETOPT(ap, alcove_opt_exit_status, 1);
+
+  if (getrlimit(RLIMIT_NOFILE, &maxfd) < 0)
+    exit(errno);
+
+  ap->maxfd = maxfd.rlim_cur;
+  ap->fdsetsize = ALCOVE_MAXCHILD(ap->maxfd);
+  ap->maxforkdepth = MAXFORKDEPTH;
+  ap->flowcontrol = -1;
+  ap->signaloneof = SIGTERM;
+
+  while ((ch = getopt(argc, argv, "c:d:h")) != -1) {
+    switch (ch) {
+    case 'c':
+      if (fifo)
+        free(fifo);
+      fifo = strdup(optarg);
+      if (fifo == NULL)
         exit(ENOMEM);
-
-    ALCOVE_SETOPT(ap, alcove_opt_termsig, 1);
-    ALCOVE_SETOPT(ap, alcove_opt_exit_status, 1);
-
-    if (getrlimit(RLIMIT_NOFILE, &maxfd) < 0)
-        exit(errno);
-
-    ap->maxfd = maxfd.rlim_cur;
-    ap->fdsetsize = ALCOVE_MAXCHILD(ap->maxfd);
-    ap->maxforkdepth = MAXFORKDEPTH;
-    ap->flowcontrol = -1;
-    ap->signaloneof = SIGTERM;
-
-    while ( (ch = getopt(argc, argv, "c:d:h")) != -1) {
-        switch (ch) {
-            case 'c':
-                if (fifo) free(fifo);
-                fifo = strdup(optarg);
-                if (fifo == NULL)
-                    exit(ENOMEM);
-                break;
-            case 'd':
-                boot = 0;
-                ap->depth = (u_int16_t)atoi(optarg);
-                if (ap->depth > UINT8_MAX)
-                    exit(EAGAIN);
-                break;
-            case 'h':
-            default:
-                usage();
-        }
+      break;
+    case 'd':
+      boot = 0;
+      ap->depth = (u_int16_t)atoi(optarg);
+      if (ap->depth > UINT8_MAX)
+        exit(EAGAIN);
+      break;
+    case 'h':
+    default:
+      usage();
     }
+  }
 
-    ap->child = calloc(ap->fdsetsize, sizeof(alcove_child_t));
-    if (ap->child == NULL)
-        exit(ENOMEM);
+  ap->child = calloc(ap->fdsetsize, sizeof(alcove_child_t));
+  if (ap->child == NULL)
+    exit(ENOMEM);
 
-    if (alcove_signal_init(boot) < 0)
-        exit(errno);
+  if (alcove_signal_init(boot) < 0)
+    exit(errno);
 
-    if (boot) {
+  if (boot) {
 
-        if (alcove_rlimit_init() < 0)
-            exit(errno);
+    if (alcove_rlimit_init() < 0)
+      exit(errno);
 
-        if (alcove_fd_init(fifo) < 0)
-            exit(errno);
-    }
+    if (alcove_fd_init(fifo) < 0)
+      exit(errno);
+  }
 
-    free(fifo);
+  free(fifo);
 
-    alcove_event_init(ap);
-    exit(0);
+  alcove_event_init(ap);
+  exit(0);
 }
 
-    static int
-alcove_signal_init(int boot)
-{
-    struct sigaction act = {0};
-    int sig = 0;
+static int alcove_signal_init(int boot) {
+  struct sigaction act = {0};
+  int sig = 0;
 
-    act.sa_handler = SIG_DFL;
-    (void)sigfillset(&act.sa_mask);
+  act.sa_handler = SIG_DFL;
+  (void)sigfillset(&act.sa_mask);
 
-    if (boot) {
-        for (sig = 1; sig < NSIG; sig++) {
-            if (sigaction(sig, &act, NULL) < 0) {
-                if (errno == EINVAL)
-                    continue;
+  if (boot) {
+    for (sig = 1; sig < NSIG; sig++) {
+      if (sigaction(sig, &act, NULL) < 0) {
+        if (errno == EINVAL)
+          continue;
 
-                return -1;
-            }
-        }
-    }
-
-    act.sa_flags |= SA_SIGINFO;
-    act.sa_sigaction = alcove_sig_info;
-
-    if (sigaction(SIGCHLD, &act, NULL) < 0)
         return -1;
+      }
+    }
+  }
 
+  act.sa_flags |= SA_SIGINFO;
+  act.sa_sigaction = alcove_sig_info;
+
+  if (sigaction(SIGCHLD, &act, NULL) < 0)
+    return -1;
+
+  return 0;
+}
+
+static int alcove_rlimit_init(void) {
+  struct rlimit stack_size = {0};
+
+  if (getrlimit(RLIMIT_STACK, &stack_size) < 0)
+    return -1;
+
+  /* Reset an unlimited stack size to a default value. The default is
+   * set to 8Mb, the default for linux (_STK_LIM in linux/resources.h).
+   *
+   * The current value of RLIMIT_STACK is used for allocating the
+   * stack of cloned processes.
+   *
+   */
+
+  if (stack_size.rlim_cur == RLIM_INFINITY) {
+    stack_size.rlim_cur = 8 * 1024 * 1024;
+    if (setrlimit(RLIMIT_STACK, &stack_size) < 0)
+      return -1;
+  }
+
+  return 0;
+}
+
+static int alcove_fd_init(char *fifo) {
+  int sigpipe[2] = {0};
+  int fdctl = 0;
+
+  if (!fifo)
+    return -1;
+
+  /* The fd may have been opened by another program. For example,
+   * valgrind will use the first available fd for the log file. */
+  if (alcove_fdmove(ALCOVE_SIGREAD_FILENO, 8) < 0)
+    return -1;
+
+  if (alcove_fdmove(ALCOVE_SIGWRITE_FILENO, 8) < 0)
+    return -1;
+
+  if (alcove_fdmove(ALCOVE_FDCTL_FILENO, 8) < 0)
+    return -1;
+
+  if (pipe(sigpipe) < 0)
+    return -1;
+
+  /* XXX fd's will overlap */
+  if (sigpipe[0] != ALCOVE_SIGREAD_FILENO) {
+    if (dup2(sigpipe[0], ALCOVE_SIGREAD_FILENO) < 0)
+      return -1;
+    if (close(sigpipe[0]) < 0)
+      return -1;
+  }
+
+  if (sigpipe[1] != ALCOVE_SIGWRITE_FILENO) {
+    if (dup2(sigpipe[1], ALCOVE_SIGWRITE_FILENO) < 0)
+      return -1;
+    if (close(sigpipe[1]) < 0)
+      return -1;
+  }
+
+  if ((alcove_setfd(ALCOVE_SIGREAD_FILENO, FD_CLOEXEC | O_NONBLOCK) < 0) ||
+      (alcove_setfd(ALCOVE_SIGWRITE_FILENO, FD_CLOEXEC | O_NONBLOCK) < 0))
+    return -1;
+
+  /* The control fd used to signal that the port has called exec(). The
+   * control fd is a fifo. beam opens the fd in read-only mode.  When all
+   * the writers of a fifo call close(), the reader receives EOF.
+   *
+   * If the fifo is opened O_WRONLY, the port will deadlock: the open
+   * call in the port will block for a reader and the erlang side will
+   * be blocked waiting for the port.
+   *
+   * Some OS'es (e.g., Linux, see fifo(7)) allow a fifo to be opened
+   * in non-blocking mode but the behaviour is unspecified. In practice:
+   *
+   * - opening the fifo read-only returns immediately
+   *
+   * - opening the fifo write-only fifo returns ENXIO
+   *
+   * - opening the fifo read-write with or without O_NONBLOCK will
+   *   return immediately but the control fd cannot be used for sending
+   *   data because each of the readers will race to receive any writes
+   *
+   */
+  if (mkfifo(fifo, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH) < 0)
+    return -1;
+
+  fdctl = open(fifo, O_RDWR | O_CLOEXEC | O_NONBLOCK);
+  if (fdctl < 0)
+    return -1;
+
+  if (fdctl != ALCOVE_FDCTL_FILENO) {
+    if (dup2(fdctl, ALCOVE_FDCTL_FILENO) < 0)
+      return -1;
+    if (close(fdctl) < 0)
+      return -1;
+  }
+
+  return 0;
+}
+
+static int alcove_fdmove(int fd, int dst) {
+  int flags = 0;
+
+  flags = fcntl(fd, F_GETFD);
+
+  if (flags < 0)
     return 0;
+
+  if (fcntl(fd, F_DUPFD, dst) < 0)
+    return -1;
+
+  /* According to fcntl(2) on FreeBSD, the close-on-exec flag is reset
+   * by F_DUPFD:
+   *
+   * The close-on-exec flag FD_CLOEXEC associated
+   * with the new file descriptor is cleared, so the
+   * file descriptor is to remain open across
+   * execve(2) system calls.
+   *
+   * Restore the flag if it was set:
+   *
+   */
+  return fcntl(dst, F_SETFD, flags);
 }
 
-    static int
-alcove_rlimit_init(void)
-{
-    struct rlimit stack_size = {0};
+static void usage() {
+  (void)fprintf(stderr, "%s %s\n", __progname, ALCOVE_VERSION);
+  (void)fprintf(stderr, "usage: %s -c <path> [<options>]\n", __progname);
 
-    if (getrlimit(RLIMIT_STACK, &stack_size) < 0)
-        return -1;
-
-    /* Reset an unlimited stack size to a default value. The default is
-     * set to 8Mb, the default for linux (_STK_LIM in linux/resources.h).
-     *
-     * The current value of RLIMIT_STACK is used for allocating the
-     * stack of cloned processes.
-     *
-     */
-
-    if (stack_size.rlim_cur == RLIM_INFINITY) {
-        stack_size.rlim_cur = 8 * 1024 * 1024;
-        if (setrlimit(RLIMIT_STACK, &stack_size) < 0)
-            return -1;
-    }
-
-    return 0;
-}
-
-    static int
-alcove_fd_init(char *fifo)
-{
-    int sigpipe[2] = {0};
-    int fdctl = 0;
-
-    if (!fifo)
-        return -1;
-
-     /* The fd may have been opened by another program. For example,
-      * valgrind will use the first available fd for the log file. */
-    if (alcove_fdmove(ALCOVE_SIGREAD_FILENO, 8) < 0)
-        return -1;
-
-    if (alcove_fdmove(ALCOVE_SIGWRITE_FILENO, 8) < 0)
-        return -1;
-
-    if (alcove_fdmove(ALCOVE_FDCTL_FILENO, 8) < 0)
-        return -1;
-
-    if (pipe(sigpipe) < 0)
-        return -1;
-
-    /* XXX fd's will overlap */
-    if (sigpipe[0] != ALCOVE_SIGREAD_FILENO) {
-        if (dup2(sigpipe[0], ALCOVE_SIGREAD_FILENO) < 0)
-            return -1;
-        if (close(sigpipe[0]) < 0)
-            return -1;
-    }
-
-    if (sigpipe[1] != ALCOVE_SIGWRITE_FILENO) {
-        if (dup2(sigpipe[1], ALCOVE_SIGWRITE_FILENO) < 0)
-            return -1;
-        if (close(sigpipe[1]) < 0)
-            return -1;
-    }
-
-    if ( (alcove_setfd(ALCOVE_SIGREAD_FILENO, FD_CLOEXEC|O_NONBLOCK) < 0)
-            || (alcove_setfd(ALCOVE_SIGWRITE_FILENO, FD_CLOEXEC|O_NONBLOCK) < 0))
-        return -1;
-
-    /* The control fd used to signal that the port has called exec(). The
-     * control fd is a fifo. beam opens the fd in read-only mode.  When all
-     * the writers of a fifo call close(), the reader receives EOF.
-     *
-     * If the fifo is opened O_WRONLY, the port will deadlock: the open
-     * call in the port will block for a reader and the erlang side will
-     * be blocked waiting for the port.
-     *
-     * Some OS'es (e.g., Linux, see fifo(7)) allow a fifo to be opened
-     * in non-blocking mode but the behaviour is unspecified. In practice:
-     *
-     * - opening the fifo read-only returns immediately
-     *
-     * - opening the fifo write-only fifo returns ENXIO
-     *
-     * - opening the fifo read-write with or without O_NONBLOCK will
-     *   return immediately but the control fd cannot be used for sending
-     *   data because each of the readers will race to receive any writes
-     *
-     */
-    if (mkfifo(fifo, S_IWUSR|S_IRUSR|S_IRGRP|S_IROTH) < 0)
-        return -1;
-
-    fdctl = open(fifo, O_RDWR|O_CLOEXEC|O_NONBLOCK);
-    if (fdctl < 0)
-        return -1;
-
-    if (fdctl != ALCOVE_FDCTL_FILENO) {
-        if (dup2(fdctl, ALCOVE_FDCTL_FILENO) < 0)
-            return -1;
-        if (close(fdctl) < 0)
-            return -1;
-    }
-
-    return 0;
-}
-
-    static int
-alcove_fdmove(int fd, int dst)
-{
-    int flags = 0;
-
-    flags = fcntl(fd, F_GETFD);
-
-    if (flags < 0)
-        return 0;
-
-    if (fcntl(fd, F_DUPFD, dst) < 0)
-        return -1;
-
-    /* According to fcntl(2) on FreeBSD, the close-on-exec flag is reset
-     * by F_DUPFD:
-     *
-     * The close-on-exec flag FD_CLOEXEC associated
-     * with the new file descriptor is cleared, so the
-     * file descriptor is to remain open across
-     * execve(2) system calls.
-     *
-     * Restore the flag if it was set:
-     *
-     */
-    return fcntl(dst, F_SETFD, flags);
-}
-
-    static void
-usage()
-{
-    (void)fprintf(stderr, "%s %s\n",
-            __progname, ALCOVE_VERSION);
-    (void)fprintf(stderr,
-            "usage: %s -c <path> [<options>]\n",
-            __progname
-            );
-
-    exit (ENOTSUP);
+  exit(ENOTSUP);
 }
--- c_src/alcove.h
+++ c_src/alcove.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2017, Michael Santos <michael.santos@gmail.com>
+/* Copyright (c) 2014-2020, Michael Santos <michael.santos@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -16,17 +16,17 @@
 #define _GNU_SOURCE
 #endif
 
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/uio.h>
+#include <limits.h>
+#include <signal.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/param.h>
 #include <sys/resource.h>
-#include <sys/types.h>
-#include <signal.h>
 #include <sys/socket.h>
-#include <limits.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
 
 #include <arpa/inet.h>
 #include <stdint.h>
@@ -36,147 +36,155 @@
 #include <ei.h>
 
 #include "erl_errno.h"
-#include "alcove_version.h"
+
 #include "alcove_nr.h"
+#include "alcove_version.h"
 
 #include "reallocarray.h"
 #include "recallocarray.h"
 #include "setproctitle.h"
 
 #if defined(__sun) && defined(__SVR4)
-#define u_int8_t        uint8_t
-#define u_int16_t       uint16_t
-#define u_int32_t       uint32_t
-#define u_int64_t       uint64_t
+#define u_int8_t uint8_t
+#define u_int16_t uint16_t
+#define u_int32_t uint32_t
+#define u_int64_t uint64_t
 
 #define __sunos__
 #endif
 
 #ifndef MAX
-#define MAX(x,y) ((x) > (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 #endif
 
 #ifndef MIN
-#define MIN(x,y) ((x) < (y) ? (x) : (y))
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
 #endif
 
 #define UNUSED(x) (void)x
 
-#define MAXFORKDEPTH    16
-#define MAXMSGLEN       UINT16_MAX
-#define MAXHDRLEN       8 /* 2 bytes length + 2 bytes type + 4 bytes PID */
+#define MAXFORKDEPTH 16
+#define MAXMSGLEN UINT16_MAX
+#define MAXHDRLEN 8 /* 2 bytes length + 2 bytes type + 4 bytes PID */
 
-#define ALCOVE_MSGLEN(x,n) \
-    ((n) - (((x) + 1) * MAXHDRLEN))
+#define ALCOVE_MSGLEN(x, n) ((n) - (((x) + 1) * MAXHDRLEN))
 
-#define ALCOVE_CONSTANT(x) {#x, x}
+#define ALCOVE_CONSTANT(x)                                                     \
+  { #x, x }
 
-#define ALCOVE_SETOPT(x,k,v) \
-    (x)->opt = (v) ? (x)->opt | (k) : (x)->opt & ~(k)
+#define ALCOVE_SETOPT(x, k, v) (x)->opt = (v) ? (x)->opt | (k) : (x)->opt & ~(k)
 
-#define ALCOVE_ERR(_x) \
-    if ((_x) < 0) abort();
+#define ALCOVE_ERR(_x)                                                         \
+  if ((_x) < 0)                                                                \
+    abort();
 
-#define ALCOVE_TUPLE2(_msg, _len, _index, _tag, _term) do { \
-    *(_index) = 0; \
-    ALCOVE_ERR(alcove_encode_version(_msg, _len, _index)); \
-    ALCOVE_ERR(alcove_encode_tuple_header(_msg, _len, _index, 2)); \
-    ALCOVE_ERR(alcove_encode_atom(_msg, _len, _index, _tag)); \
-    ALCOVE_ERR(_term); \
-    } while (0)
+#define ALCOVE_TUPLE2(_msg, _len, _index, _tag, _term)                         \
+  do {                                                                         \
+    *(_index) = 0;                                                             \
+    ALCOVE_ERR(alcove_encode_version(_msg, _len, _index));                     \
+    ALCOVE_ERR(alcove_encode_tuple_header(_msg, _len, _index, 2));             \
+    ALCOVE_ERR(alcove_encode_atom(_msg, _len, _index, _tag));                  \
+    ALCOVE_ERR(_term);                                                         \
+  } while (0)
 
-#define ALCOVE_TUPLE3(_msg, _len, _index, _tag, _term1, _term2) do { \
-    *(_index) = 0; \
-    ALCOVE_ERR(alcove_encode_version(_msg, _len, _index)); \
-    ALCOVE_ERR(alcove_encode_tuple_header(_msg, _len, _index, 3)); \
-    ALCOVE_ERR(alcove_encode_atom(_msg, _len, _index, _tag)); \
-    ALCOVE_ERR(_term1); \
-    ALCOVE_ERR(_term2); \
-    } while (0)
+#define ALCOVE_TUPLE3(_msg, _len, _index, _tag, _term1, _term2)                \
+  do {                                                                         \
+    *(_index) = 0;                                                             \
+    ALCOVE_ERR(alcove_encode_version(_msg, _len, _index));                     \
+    ALCOVE_ERR(alcove_encode_tuple_header(_msg, _len, _index, 3));             \
+    ALCOVE_ERR(alcove_encode_atom(_msg, _len, _index, _tag));                  \
+    ALCOVE_ERR(_term1);                                                        \
+    ALCOVE_ERR(_term2);                                                        \
+  } while (0)
 
-#define ALCOVE_TUPLE4(_msg, _len, _index, _tag, _term1, _term2, _term3) do { \
-    *(_index) = 0; \
-    ALCOVE_ERR(alcove_encode_version(_msg, _len, _index)); \
-    ALCOVE_ERR(alcove_encode_tuple_header(_msg, _len, _index, 4)); \
-    ALCOVE_ERR(alcove_encode_atom(_msg, _len, _index, _tag)); \
-    ALCOVE_ERR(_term1); \
-    ALCOVE_ERR(_term2); \
-    ALCOVE_ERR(_term3); \
-    } while (0)
+#define ALCOVE_TUPLE4(_msg, _len, _index, _tag, _term1, _term2, _term3)        \
+  do {                                                                         \
+    *(_index) = 0;                                                             \
+    ALCOVE_ERR(alcove_encode_version(_msg, _len, _index));                     \
+    ALCOVE_ERR(alcove_encode_tuple_header(_msg, _len, _index, 4));             \
+    ALCOVE_ERR(alcove_encode_atom(_msg, _len, _index, _tag));                  \
+    ALCOVE_ERR(_term1);                                                        \
+    ALCOVE_ERR(_term2);                                                        \
+    ALCOVE_ERR(_term3);                                                        \
+  } while (0)
 
-#define ALCOVE_OK(_msg, _len, _index, _buf) \
-    ALCOVE_TUPLE2(_msg, _len, _index, "ok", _buf)
+#define ALCOVE_OK(_msg, _len, _index, _buf)                                    \
+  ALCOVE_TUPLE2(_msg, _len, _index, "ok", _buf)
 
-#define get_int32(s) ((((const unsigned char*) (s))[0] << 24) | \
-                      (((const unsigned char*) (s))[1] << 16) | \
-                      (((const unsigned char*) (s))[2] << 8)  | \
-                      (((const unsigned char*) (s))[3]))
+#define get_int32(s)                                                           \
+  ((((const unsigned char *)(s))[0] << 24) |                                   \
+   (((const unsigned char *)(s))[1] << 16) |                                   \
+   (((const unsigned char *)(s))[2] << 8) | (((const unsigned char *)(s))[3]))
 
-#define put_int32(i, s) do {((char*)(s))[0] = (char)((i) >> 24) & 0xff;   \
-                            ((char*)(s))[1] = (char)((i) >> 16) & 0xff;   \
-                            ((char*)(s))[2] = (char)((i) >> 8)  & 0xff;   \
-                            ((char*)(s))[3] = (char)(i)         & 0xff;} \
-                        while (0)
+#define put_int32(i, s)                                                        \
+  do {                                                                         \
+    ((char *)(s))[0] = (char)((i) >> 24) & 0xff;                               \
+    ((char *)(s))[1] = (char)((i) >> 16) & 0xff;                               \
+    ((char *)(s))[2] = (char)((i) >> 8) & 0xff;                                \
+    ((char *)(s))[3] = (char)(i)&0xff;                                         \
+  } while (0)
 
-#define get_int16(s) ((((const unsigned char*)  (s))[0] << 8) | \
-                      (((const unsigned char*)  (s))[1]))
+#define get_int16(s)                                                           \
+  ((((const unsigned char *)(s))[0] << 8) | (((const unsigned char *)(s))[1]))
 
-#define put_int16(i, s) do {((char*)(s))[0] = (char)((i) >> 8) & 0xff;  \
-                            ((char*)(s))[1] = (char)(i)        & 0xff;} \
-                        while (0)
+#define put_int16(i, s)                                                        \
+  do {                                                                         \
+    ((char *)(s))[0] = (char)((i) >> 8) & 0xff;                                \
+    ((char *)(s))[1] = (char)(i)&0xff;                                         \
+  } while (0)
 
-#define get_int8(s) (((const unsigned char*)  (s))[0])
+#define get_int8(s) (((const unsigned char *)(s))[0])
 
 enum {
-    alcove_opt_stdin_closed = 1 << 0,  /* Report child stdin closed */
-    alcove_opt_stdout_closed = 1 << 1, /* Report child stdout closed */
-    alcove_opt_stderr_closed = 1 << 2, /* Report child stderr closed */
-    alcove_opt_exit_status = 1 << 3,   /* Report child exit status */
-    alcove_opt_termsig = 1 << 4,       /* Report child termination signal */
-    alcove_opt_signaloneof = 1 << 5    /* Signal child processes on EOF */
+  alcove_opt_stdin_closed = 1 << 0,  /* Report child stdin closed */
+  alcove_opt_stdout_closed = 1 << 1, /* Report child stdout closed */
+  alcove_opt_stderr_closed = 1 << 2, /* Report child stderr closed */
+  alcove_opt_exit_status = 1 << 3,   /* Report child exit status */
+  alcove_opt_termsig = 1 << 4,       /* Report child termination signal */
+  alcove_opt_signaloneof = 1 << 5    /* Signal child processes on EOF */
 };
 
 enum {
-    ALCOVE_SIGREAD_FILENO = 3,
-    ALCOVE_SIGWRITE_FILENO,
-    ALCOVE_FDCTL_FILENO,
-    ALCOVE_MAXFILENO
+  ALCOVE_SIGREAD_FILENO = 3,
+  ALCOVE_SIGWRITE_FILENO,
+  ALCOVE_FDCTL_FILENO,
+  ALCOVE_MAXFILENO
 };
 
 #define ALCOVE_MAXCHILD(_nfds) ((_nfds) / ALCOVE_MAXFILENO - ALCOVE_MAXFILENO)
 
 typedef struct {
-    pid_t pid;
-    int flowcontrol;
-    int signaloneof;
-    int exited;
-    int fdctl;
-    int fdin;
-    int fdout;
-    int fderr;
+  pid_t pid;
+  int flowcontrol;
+  int signaloneof;
+  int exited;
+  int fdctl;
+  int fdin;
+  int fdout;
+  int fderr;
 } alcove_child_t;
 
 typedef struct {
-    int32_t opt;
-    rlim_t maxfd;
-    u_int8_t sigchld;
-    u_int16_t maxforkdepth;
-    u_int16_t fdsetsize;
-    u_int16_t depth;
-    int flowcontrol;
-    int signaloneof;
-    alcove_child_t *child;
-    uint8_t filter[ALCOVE_NR_SIZE];
+  int32_t opt;
+  rlim_t maxfd;
+  u_int8_t sigchld;
+  u_int16_t maxforkdepth;
+  u_int16_t fdsetsize;
+  u_int16_t depth;
+  int flowcontrol;
+  int signaloneof;
+  alcove_child_t *child;
+  uint8_t filter[ALCOVE_NR_SIZE];
 } alcove_state_t;
 
 typedef struct {
-    char *name;
-    long long val;
+  char *name;
+  long long val;
 } alcove_constant_t;
 
 typedef struct {
-    void *p;
-    size_t len;
+  void *p;
+  size_t len;
 } alcove_alloc_t;
 
 void alcove_sig_info(int sig, siginfo_t *info, void *context);
@@ -185,8 +193,8 @@ void alcove_event_init(alcove_state_t *ap);
 void alcove_event_loop(alcove_state_t *ap);
 
 int pid_foreach(alcove_state_t *ap, pid_t pid, void *arg1, void *arg2,
-        int (*comp)(pid_t, pid_t),
-        int (*fp)(alcove_state_t *, alcove_child_t *, void *, void *));
+                int (*comp)(pid_t, pid_t),
+                int (*fp)(alcove_state_t *, alcove_child_t *, void *, void *));
 int pid_equal(pid_t p1, pid_t p2);
 int pid_not_equal(pid_t p1, pid_t p2);
 
@@ -207,17 +215,17 @@ int alcove_decode_list_header(const char *, size_t, int *, int *);
 int alcove_decode_tuple_header(const char *, size_t, int *, int *);
 int alcove_decode_iolist(const char *, size_t, int *, char *, size_t *);
 int alcove_decode_constant(const char *, size_t, int *, int *,
-        const alcove_constant_t *);
+                           const alcove_constant_t *);
 int alcove_decode_constant_list(const char *, size_t, int *, int *,
-        const alcove_constant_t *);
+                                const alcove_constant_t *);
 int alcove_decode_constant64(const char *buf, size_t len, int *index,
-        long long *val, const alcove_constant_t *constants);
+                             long long *val,
+                             const alcove_constant_t *constants);
 int alcove_decode_cstruct(const char *, size_t, int *, char *, size_t *,
-        alcove_alloc_t **, ssize_t *);
+                          alcove_alloc_t **, ssize_t *);
 int alcove_decode_argv(const char *, size_t, int *, char ***);
 void alcove_free_argv(char **);
 int alcove_decode_null(const char *buf, size_t len, int *index);
-
 
 int alcove_encode_version(char *, size_t, int *);
 int alcove_encode_list_header(char *, size_t, int *, int);
@@ -230,12 +238,12 @@ int alcove_encode_ulonglong(char *, size_t, int *, unsigned long long);
 int alcove_encode_atom(char *, size_t, int *, const char *);
 int alcove_encode_binary(char *, size_t, int *, const void *, long);
 int alcove_encode_constant(char *, size_t, int *, char *,
-        const alcove_constant_t *);
+                           const alcove_constant_t *);
 int alcove_lookup_constant(char *, long long *, const alcove_constant_t *);
 int alcove_encode_constant_id(char *, size_t, int *, long long,
-        const alcove_constant_t *);
+                              const alcove_constant_t *);
 int alcove_encode_cstruct(char *, size_t, int *, const char *, size_t,
-        alcove_alloc_t *, ssize_t);
+                          alcove_alloc_t *, ssize_t);
 
 ssize_t alcove_mk_errno(char *buf, size_t len, int errnum);
 ssize_t alcove_mk_error(char *buf, size_t len, const char *reason);
@@ -244,5 +252,5 @@ ssize_t alcove_mk_binary(char *, size_t, const void *, size_t);
 ssize_t alcove_mk_long(char *, size_t, long);
 ssize_t alcove_mk_ulong(char *, size_t, unsigned long);
 
-ssize_t alcove_call(alcove_state_t *ap, u_int32_t call,
-        const char *arg, size_t len, char *reply, size_t rlen);
+ssize_t alcove_call(alcove_state_t *ap, u_int32_t call, const char *arg,
+                    size_t len, char *reply, size_t rlen);
