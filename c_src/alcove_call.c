9244b7031f209cf383f1144b9f6f6f26707e897a
Michael Santos <michael.santos@gmail.com>
2020-03-13 10:14:51 -0400

    alcove_call.c: reformat, prevent header re-ordering

--- c_src/alcove_call.c
+++ c_src/alcove_call.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2018, Michael Santos <michael.santos@gmail.com>
+/* Copyright (c) 2014-2020, Michael Santos <michael.santos@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -13,61 +13,60 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 #include "alcove.h"
+
 #include "alcove_call.h"
+
 #include "alcove_calls.h"
 
-    ssize_t
-alcove_call(alcove_state_t *ap, u_int32_t call,
-        const char *arg, size_t len,
-        char *reply, size_t rlen)
-{
-    int index = 0;
-    int arity = 0;
-    int version = 0;
+ssize_t alcove_call(alcove_state_t *ap, u_int32_t call, const char *arg,
+                    size_t len, char *reply, size_t rlen) {
+  int index = 0;
+  int arity = 0;
+  int version = 0;
 
-    const alcove_call_t *fun = NULL;
-    ssize_t written = 0;
+  const alcove_call_t *fun = NULL;
+  ssize_t written = 0;
 
-    uint8_t j;
-    uint8_t k;
+  uint8_t j;
+  uint8_t k;
 
-    if (call >= sizeof(calls)/sizeof(calls[0]))
-        goto BADARG;
+  if (call >= sizeof(calls) / sizeof(calls[0]))
+    goto BADARG;
 
-    j = call / 8;
-    k = call % 8;
+  j = call / 8;
+  k = call % 8;
 
-    if (ap->filter[j] & (1 << k))
-      goto UNDEF;
+  if (ap->filter[j] & (1 << k))
+    goto UNDEF;
 
-    fun = &calls[call];
+  fun = &calls[call];
 
-    /* minimum input in external term format
-     * Magic:1/bytes, SmallTupleHeader:1/bytes, Arity:1/bytes
-     * <<131,104,0>>
-     */
-    if (len < 4)
-        goto BADARG;
+  /* minimum input in external term format
+   * Magic:1/bytes, SmallTupleHeader:1/bytes, Arity:1/bytes
+   * <<131,104,0>>
+   */
+  if (len < 4)
+    goto BADARG;
 
-    if (ei_decode_version(arg, &index, &version) < 0)
-        goto BADARG;
+  if (ei_decode_version(arg, &index, &version) < 0)
+    goto BADARG;
 
-    if (alcove_decode_tuple_header(arg, len, &index, &arity) < 0)
-        goto BADARG;
+  if (alcove_decode_tuple_header(arg, len, &index, &arity) < 0)
+    goto BADARG;
 
-    if (arity != fun->narg)
-        goto BADARG;
+  if (arity != fun->narg)
+    goto BADARG;
 
-    written = (*fun->fp)(ap, arg+index, len-index, reply, rlen);
+  written = (*fun->fp)(ap, arg + index, len - index, reply, rlen);
 
-    if (written < 0)
-        goto BADARG;
+  if (written < 0)
+    goto BADARG;
 
-    return written;
+  return written;
 
 BADARG:
-    return alcove_mk_atom(reply, rlen, "badarg");
+  return alcove_mk_atom(reply, rlen, "badarg");
 
 UNDEF:
-    return alcove_mk_atom(reply, rlen, "undef");
+  return alcove_mk_atom(reply, rlen, "undef");
 }
