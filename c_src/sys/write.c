56114a1f82cd0126b3e4c8247c7d2e38e7719c07
Michael Santos <michael.santos@gmail.com>
2024-01-26 08:09:04 -0500

    write: var initialization

--- c_src/sys/write.c
+++ c_src/sys/write.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, Michael Santos <michael.santos@gmail.com>
+/* Copyright (c) 2014-2024, Michael Santos <michael.santos@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -27,7 +27,7 @@ ssize_t alcove_sys_write(alcove_state_t *ap, const char *arg, size_t len,
   int fd = -1;
   char buf[MAXMSGLEN] = {0};
   size_t buflen = sizeof(buf);
-  int rv = 0;
+  int rv;
 
   UNUSED(ap);
 
