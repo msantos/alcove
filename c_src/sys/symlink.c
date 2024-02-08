0517cfbc4155d7a8859bbf05469db5d7734a1942
Michael Santos <michael.santos@gmail.com>
2024-02-08 07:42:39 -0500

    symlink: cleanup: var initialization

--- c_src/sys/symlink.c
+++ c_src/sys/symlink.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015, Michael Santos <michael.santos@gmail.com>
+/* Copyright (c) 2015-2024, Michael Santos <michael.santos@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -26,7 +26,7 @@ ssize_t alcove_sys_symlink(alcove_state_t *ap, const char *arg, size_t len,
   char newpath[PATH_MAX] = {0};
   size_t oldpathlen = sizeof(oldpath) - 1;
   size_t newpathlen = sizeof(newpath) - 1;
-  int rv = 0;
+  int rv;
 
   UNUSED(ap);
 
