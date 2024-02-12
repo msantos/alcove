0576dfc93627ac242d626cfbce8ff406ac1b65ac
Michael Santos <michael.santos@gmail.com>
2024-02-12 10:09:57 -0500

    socket: cleanup: var initialization

--- c_src/sys/socket.c
+++ c_src/sys/socket.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016, Michael Santos <michael.santos@gmail.com>
+/* Copyright (c) 2016-2024, Michael Santos <michael.santos@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -32,7 +32,7 @@ ssize_t alcove_sys_socket(alcove_state_t *ap, const char *arg, size_t len,
   int type = 0;
   int protocol = 0;
 
-  int fd = 0;
+  int fd;
 
   UNUSED(ap);
 
