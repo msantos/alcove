61eb19892990deb14dd8d45ebbdc72d4c2e18719
Michael Santos <michael.santos@gmail.com>
2024-02-14 07:26:39 -0500

    setrlimit: cleanup: var initialization

--- c_src/sys/setrlimit.c
+++ c_src/sys/setrlimit.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2022, Michael Santos <michael.santos@gmail.com>
+/* Copyright (c) 2014-2024, Michael Santos <michael.santos@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -30,7 +30,7 @@ ssize_t alcove_sys_setrlimit(alcove_state_t *ap, const char *arg, size_t len,
   char atom[MAXATOMLEN] = {0};
   unsigned long long cur = 0, max = 0;
   struct rlimit rlim = {0};
-  int rv = 0;
+  int rv;
   size_t nproc = 0;
 
   UNUSED(ap);
