f9f343153a1ac0421cef46ce327fc5e3bb1080e0
Michael Santos <michael.santos@gmail.com>
2024-07-31 07:19:02 -0400

    ptrace: var reinitialization, copyright date

--- c_src/sys/ptrace.c
+++ c_src/sys/ptrace.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016-2017, Michael Santos <michael.santos@gmail.com>
+/* Copyright (c) 2016-2024, Michael Santos <michael.santos@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -57,7 +57,7 @@ ssize_t alcove_sys_ptrace(alcove_state_t *ap, const char *arg, size_t len,
   alcove_alloc_t *elem = NULL;
   ssize_t nelem = 0;
 
-  long rv = 0;
+  long rv;
 
   UNUSED(ap);
 
