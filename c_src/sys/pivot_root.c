cb0d8bfb9cf7d1ab391b98397b3cfe603a443d2b
Michael Santos <michael.santos@gmail.com>
2024-08-14 06:52:46 -0400

    pivot_root: var reinitialization, copyright date

--- c_src/sys/pivot_root.c
+++ c_src/sys/pivot_root.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, Michael Santos <michael.santos@gmail.com>
+/* Copyright (c) 2014-2024, Michael Santos <michael.santos@gmail.com>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -31,7 +31,7 @@ ssize_t alcove_sys_pivot_root(alcove_state_t *ap, const char *arg, size_t len,
   char put_old[PATH_MAX] = {0};
   size_t nlen = sizeof(new_root) - 1;
   size_t plen = sizeof(put_old) - 1;
-  int rv = 0;
+  int rv;
 
   UNUSED(ap);
 
