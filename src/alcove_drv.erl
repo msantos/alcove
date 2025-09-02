a384b3bb1cf6ab469b8539b4d97f74b12ca02e0a
Michael Santos <michael.santos@gmail.com>
2025-09-02 07:26:21 -0400

    doc: update copyright date

--- src/alcove_drv.erl
+++ src/alcove_drv.erl
@@ -1,4 +1,4 @@
-%%% Copyright (c) 2014-2022, Michael Santos <michael.santos@gmail.com>
+%%% Copyright (c) 2014-2025, Michael Santos <michael.santos@gmail.com>
 %%%
 %%% Permission to use, copy, modify, and/or distribute this software for any
 %%% purpose with or without fee is hereby granted, provided that the above
